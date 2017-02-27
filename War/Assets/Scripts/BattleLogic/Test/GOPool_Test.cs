5381d173703915dfa31d6efee462458f138060b5
243884919@qq.com <243884919@qq.com>
2017-02-27 10:56:44 +0800

    CharObj不带特效流程走通

---
+++ War/Assets/Scripts/BattleLogic/Test/GOPool_Test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GOPool_Test : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+    //    QObjCreatorFactory<GameObject> creatorFactory = new QObjCreatorFactoryForMeshBaker(
+    //paths, BattleScene2.E_BATTLE_OBJECT_TYPE.TANK, count);
+
+        QObjCreatorForGameObject creator = new QObjCreatorForGameObject(
+            "Effect/Tank/Prefab/Tank_dapao_fire", 32);
+
+        QObjPool<GameObject> pool = new QObjPool<GameObject>();
+
+        pool.Init(creator, null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
