30cc5dd98bb5402c248634eff7d87a46cbeefcbc
243884919@qq.com <243884919@qq.com>
2017-03-13 14:28:16 +0800

    先屏蔽在非Resources目录加载资源

--- War/Assets/Scripts/BattleLogic/Mediator/ResourcesManagerMediator.cs
+++ War/Assets/Scripts/BattleLogic/Mediator/ResourcesManagerMediator.cs
@@ -30,26 +30,5 @@ public class ResourcesManagerMediator
         go = GameObject.Instantiate<GameObject>(goRes);
 
         return go;
-
     }
-
-    public static GameObject GetGameObjectFromResourcesManager2(string path)
-    {
-        GameObject go = null;
-        GameObject goRes = null;
-        //Debug.Log(Application.dataPath);
-        string _path = "Assets/" + path;
-        goRes = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>(_path);
-        if (goRes == null)
-        {
-            Debug.LogError("资源加载错误！ " + _path);
-            return go;
-        }
-
-        go = GameObject.Instantiate<GameObject>(goRes);
-
-        return go;
-    }
-
-
 }
--- War/Assets/Scripts/Test/ResourcesManager/LoadTest_1.cs
+++ War/Assets/Scripts/Test/ResourcesManager/LoadTest_1.cs
@@ -3,10 +3,28 @@ using System.Collections;
 
 public class LoadTest_1 : MonoBehaviour 
 {
+    //public static GameObject GetGameObjectFromResourcesManager2(string path)
+    //{
+    //    GameObject go = null;
+    //    GameObject goRes = null;
+    //    //Debug.Log(Application.dataPath);
+    //    string _path = "Assets/" + path;
+    //    goRes = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>(_path);
+    //    if (goRes == null)
+    //    {
+    //        Debug.LogError("资源加载错误！ " + _path);
+    //        return go;
+    //    }
+
+    //    go = GameObject.Instantiate<GameObject>(goRes);
+
+    //    return go;
+    //}
     void Start () 
     {
-        ResourcesManagerMediator.GetGameObjectFromResourcesManager2(
-            "Scripts/Test/ResourcesManager/Tank_1.prefab");
+
+        //ResourcesManagerMediator.GetGameObjectFromResourcesManager2(
+        //    "Scripts/Test/ResourcesManager/Tank_1.prefab");
     }
 
 }
