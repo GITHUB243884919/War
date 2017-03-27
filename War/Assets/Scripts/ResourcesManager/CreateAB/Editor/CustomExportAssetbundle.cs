e45d9bdf02c79b9e5b0bba7440d1df6876cab6d6
243884919@qq.com <243884919@qq.com>
2017-03-27 19:40:01 +0800

    增加资源管理思路文档（完善中） 增加AB打包脚本

---
+++ War/Assets/Scripts/ResourcesManager/CreateAB/Editor/CustomExportAssetbundle.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+public class CustomExportAssetbundle
+{
+    [MenuItem("自定义菜单/生成所有Assetbundle")]
+    static private void BuildAssetBundles()
+    {
+        //清理资源上的AB名字
+        UntagAssetBundles();
+        //为需要打AB的资源打上名字
+        TagAssetBundles();
+        
+        //打出AB包
+        GenAssetBundles();
+
+        //为资源更新记录AssetBundle详单
+        GenDetailFile();
+    }
+
+    private static void UntagAssetBundles()
+    {
+        TagAssetBundle.UntagAssetBundles(
+            "Assets/Resources/RuntimeMeshBaker/M_Arm_Tank", "");
+    }
+    private static void TagAssetBundles()
+    {
+        TagAssetBundle.TagInDirectory(
+            "Assets/Resources/RuntimeMeshBaker/M_Arm_Tank", "", true);
+    }
+
+    private static void GenAssetBundles()
+    {
+        BuildAssetBundleOptions buildAssetBundleOptions =
+            BuildAssetBundleOptions.DisableWriteTypeTree     |
+            BuildAssetBundleOptions.DeterministicAssetBundle |
+            BuildAssetBundleOptions.ForceRebuildAssetBundle  |
+            BuildAssetBundleOptions.ChunkBasedCompression;
+        //BuildTarget buildTarget = GetBuildTarget();
+        BuildTarget buildTarget = EditorUserBuildSettings.activeBuildTarget;
+        string outputPath = Application.dataPath + "/StreamingAssets";
+        BuildPipeline.BuildAssetBundles(outputPath, buildAssetBundleOptions, buildTarget);
+    }
+
+    private static void GenDetailFile()
+    {
+        string bundleListPath = "Resources/bundlelist.txt";
+        string assetListPath  = "Resources/assetlist.txt";
+        FileStream fsBundleList = new FileStream(
+            Application.dataPath + "/" + bundleListPath, FileMode.Create);
+        FileStream fsAssetList = new FileStream(
+            Application.dataPath + "/" + assetListPath, FileMode.Create);
+
+        StreamWriter swBundleList = new StreamWriter(fsBundleList);
+        StreamWriter swAssetList = new StreamWriter(fsAssetList);
+        string lineBundleList = "";
+        string lineAssetList = "";
+        var bundles = AssetDatabase.GetAllAssetBundleNames();
+        foreach (var bundle in bundles)
+        {
+            uint crc;
+            BuildPipeline.GetCRCForAssetBundle(Application.dataPath + "/StreamingAssets/" + bundle, out crc);
+            Hash128 hash;
+            BuildPipeline.GetHashForAssetBundle(Application.dataPath + "/StreamingAssets/" + bundle, out hash);
+
+            Debug.Log("AssetBundle: " + bundle + " CRC: " + crc + " Hash:" + hash);
+            lineBundleList = string.Format("{0:S},{1:S},{2:S}", bundle.ToString(), crc.ToString(), hash.ToString());
+            //Debug.Log("format " + string.Format("%s,%s,%s,1", bundle.ToString(), crc.ToString(), hash.ToString()));
+            swBundleList.WriteLine(lineBundleList);
+
+            var assets = AssetDatabase.GetAssetPathsFromAssetBundle(bundle);
+            foreach (var asset in assets)
+            {
+                Debug.Log("Asset :" + asset);
+                string[] assetName = asset.Substring(7).Split('.');
+                lineAssetList = string.Format("{0:S},{1:S}",
+                    assetName[0], bundle);
+                swAssetList.WriteLine(lineAssetList);
+            }
+        }
+
+        if (swBundleList != null)
+        {
+            swBundleList.Dispose();
+            swBundleList = null;
+        }
+        if (fsBundleList != null)
+        {
+            fsBundleList.Dispose();
+            fsBundleList = null;
+        }
+
+        if (swAssetList != null)
+        {
+            swAssetList.Dispose();
+            swAssetList = null;
+        }
+        if (fsAssetList != null)
+        {
+            fsAssetList.Dispose();
+            fsAssetList = null;
+        }
+
+        //把详单打成AB
+        //Debug.Log("bundleListPath:" + bundleListPath);
+        //var importer = AssetImporter.GetAtPath("Assets/" + bundleListPath);
+        //importer.assetBundleName = "bundleinfo/bundlelist.bundle";
+        //var importer2 = AssetImporter.GetAtPath("Assets/" + assetListPath);
+        //importer2.assetBundleName = "bundleinfo/assetlist.bundle";
+        //BuildPipeline.BuildAssetBundles(
+        //    tagetPath, BuildAssetBundleOptions.UncompressedAssetBundle 
+        //    | BuildAssetBundleOptions.CollectDependencies, 
+        //    GetBuildTarget());
+    }
+
+    //    static private BuildTarget GetBuildTarget()
+    //    {
+    //        BuildTarget target = BuildTarget.StandaloneWindows;
+    //#if UNITY_STANDALONE
+    //        target = BuildTarget.StandaloneWindows;
+    //#elif UNITY_IPHONE
+    //            target = BuildTarget.iPhone;
+    //#elif UNITY_ANDROID
+    //            target = BuildTarget.Android;
+    //#endif
+    //        return target;
+    //    }
+}
---
+++ War/Assets/Scripts/ResourcesManager/CreateAB/Editor/TagAssetBundle.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public static class TagAssetBundle
+{
+    public static void TagInDirectory(
+        string path, string filter = "", bool useFileName = false)
+    {
+        //Debug.LogFormat("Build Texture Bundle {0}", path);
+        string assetName = GenAssetBundleName(path);
+        var files = GetAssetFiles(path, filter);
+
+        EditorUtility.DisplayProgressBar(assetName, path, 0.0f / files.Length);
+
+        int counter = 0;
+
+        foreach (var file in files)
+        {
+            if (useFileName)
+            {
+                var filename = GenAssetBundleName(
+                    file.Substring(0, file.LastIndexOf(".")));
+                assetName = GenAssetBundleName(filename);
+            }
+            TagOneAssetBundle(file, assetName, "");
+            EditorUtility.DisplayProgressBar("Build Asset Bundle (" + assetName + ")", file, (float)++counter / files.Length);
+        }
+
+
+        var dirs = AssetDatabase.GetSubFolders(path);
+        foreach (var dir in dirs)
+        {
+            TagInDirectory(dir, filter, useFileName);
+        }
+
+    }
+
+    private static string GenAssetBundleName(string path)
+    {
+        return path.Replace("Assets/", "").
+            Replace("Resources/", "").ToLower();
+    }
+
+    private static string[] GetAssetFiles(string path, string filter)
+    {
+        List<string> results = new List<string>();
+        var files = Directory.GetFiles(
+            path, "*" + filter + ".meta", SearchOption.TopDirectoryOnly);
+
+        foreach (var file in files)
+        {
+            var filepath = file.Replace(".meta", "");
+            var fileinfo = new FileInfo(filepath);
+
+            if (fileinfo.Attributes != FileAttributes.Directory 
+                && fileinfo.Attributes != FileAttributes.Hidden 
+                && !filepath.EndsWith(".cs") 
+                //&& !filepath.EndsWith(".mat") 
+                && !filepath.EndsWith(".fnt"))
+            {
+                results.Add(filepath);
+            }
+        }
+
+        return results.ToArray();
+    }
+
+    private static void TagOneAssetBundle(string filepath, string assetName, string variantName)
+    {
+
+        var importer = AssetImporter.GetAtPath(filepath);
+        if (!assetName.Equals(importer.assetBundleName))
+        {
+            importer.assetBundleName = assetName;
+            if (!string.IsNullOrEmpty(variantName))
+            {
+                importer.assetBundleVariant = variantName;
+            }
+            importer.SaveAndReimport();
+        }
+    }
+
+    public static void UntagAssetBundles(string path, string filter)
+    {
+        Debug.LogFormat("Clear Bundle {0}", path);
+
+        var files = GetAssetFiles(path, filter);
+
+        EditorUtility.DisplayProgressBar("Clear Bundle ", path, 0.0f / files.Length);
+
+        int counter = 0;
+
+        foreach (var file in files)
+        {
+            TagOneAssetBundle(file, "", "");
+            EditorUtility.DisplayProgressBar("Clear Bundle " + path, file, ++counter / files.Length);
+        }
+
+        var dirs = AssetDatabase.GetSubFolders(path);
+        foreach (var dir in dirs)
+        {
+            UntagAssetBundles(dir, filter);
+        }
+
+    }
+}
