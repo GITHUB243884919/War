22d184f992cdae04922fd9b78129b2109f694076
243884919@qq.com <243884919@qq.com>
2017-03-29 11:36:33 +0800

    增加轰炸算法（重力加速度计算速度和力）

---
+++ War/Assets/Scripts/Test/AI/Bomb/Bomb_Test_1.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb_Test_1 : MonoBehaviour 
+{
+    public  Vector3 m_target = Vector3.zero;
+    private float   m_speed = 0f;
+    private float   m_timer = 0f;
+    private float   m_interval = 0.2f;
+    private Vector3 m_force = Vector3.zero;
+    private Vector3 m_toTargetDir = Vector3.zero;
+    private float   m_nearDistance = 0f;
+    private bool    m_Active = true;
+    private Vector3 m_moveDistance = Vector3.zero;
+    private float   m_realInterval = 0f;
+    private readonly float G = 9.8f;
+
+    void Start()
+    {
+        m_toTargetDir = (m_target - transform.position).normalized;
+        //transform.LookAt(m_target);
+    }
+
+    void Update()
+    {
+        if (!m_Active)
+        {
+            return;
+        }
+
+        m_timer += Time.deltaTime;
+        if (m_timer < m_interval)
+        {
+            return;
+        }
+        m_realInterval = m_timer;
+        m_timer        = 0f;
+
+        //根据重力加速度算速度
+        m_speed       += G * m_realInterval;
+        m_force        = m_toTargetDir * m_speed;
+
+        //到达目标范围内停止
+        if (((m_target - transform.position)).magnitude <= m_nearDistance)
+        {
+            m_Active = false;
+        }
+
+        //到达目标的y值停止
+        if (transform.position.y <= m_target.y)
+        {
+            m_Active = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!m_Active)
+        {
+            return;
+        }
+
+        m_moveDistance = m_force * Time.fixedDeltaTime;
+        //Debug.Log(Time.realtimeSinceStartup + " m_moveDistance " + m_moveDistance + " m_force " + m_force);
+        transform.position += m_moveDistance;
+        //到达目标的y值停止
+        if (transform.position.y <= m_target.y)
+        {
+            m_Active = false;
+        }
+    }
+}
