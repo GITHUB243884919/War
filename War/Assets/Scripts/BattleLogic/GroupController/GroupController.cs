66b95b0de34e4ab118e9f41fa14e646d9448d056
243884919@qq.com <243884919@qq.com>
2017-03-31 18:34:15 +0800

    阵型测试进行中

--- War/Assets/Scripts/BattleLogic/GroupController/GroupController.cs
+++ War/Assets/Scripts/BattleLogic/GroupController/GroupController.cs
@@ -15,11 +15,12 @@ public class GroupController
         //HORIZONTAL_LINE, 
         //VERTICAL_LINE,   
         TARGET_VERTICAL_LINE, //面朝目标纵向
+        TARGET_TRANGLE        //面朝目标三角形
     }
 
     List<CharObj> m_charObjs = new List<CharObj>();
 
-    public void AI_Arrive(
+    public void AI_Arrive_TARGET_VERTICAL_LINE(
         GroupElement [] elments, E_FORMATION formation,
         Vector3 startPoint, Vector3 endPoint, float speed, 
         MoveSteers.StopMoveCallback callback)
@@ -39,6 +40,125 @@ public class GroupController
         }
     }
 
+    public void AI_Arrive_TARGET_TRANGLE(
+        GroupElement[] elments, E_FORMATION formation,
+        Vector3 startPoint, Vector3 endPoint, float speed,
+        MoveSteers.StopMoveCallback callback)
+    {
+        FireRange();
+    }
+
+    void FireRange()
+    {
+        float fireRange = 15f;
+        Vector3 startPos = new Vector3(0,0,32);
+        Vector3 endPos = startPos + new Vector3(fireRange, 0f, 0f);
+        LogMediator.DrawLine(startPos, endPos, Color.red, (endPos - startPos).magnitude);
+        float deg = 30f;
+        float radStart = Mathf.Deg2Rad * deg;
+        float endStart = Mathf.Deg2Rad * (deg + 90f);
+        Vector3 rayPoint1 = new Vector3(
+            fireRange * Mathf.Cos(endStart),
+            0,
+            fireRange * Mathf.Sin(endStart));
+        LogMediator.Log(rayPoint1);
+        LogMediator.DrawLine(startPos, rayPoint1, Color.blue, (rayPoint1 - startPos).magnitude);
+
+    }
+    //void FireRange2()
+    //{
+    //    Vector3 startPos = Vector3.zero;
+    //    Vector3 endPos = Vector3.zero + new Vector3(15f, 0f, 0f);
+
+    //    Vector3 frontRayPoint = transform.position + (transform.forward * fireRange);
+    //    Gizmos.color = Color.yellow;
+    //    Gizmos.DrawWireSphere(transform.position, fireRange);
+    //    Debug.DrawLine(transform.position + new Vector3(0, 2, 0), frontRayPoint + new Vector3(0, 2, 0), Color.black);
+    //    //角度转弧度
+    //    float fieldOfAttackinRadians = fieldOfAttack * 3.14f / 180.0f;
+    //    float fieldOfAttackinRadians2 = 3.14f / 2 + fieldOfAttackinRadians;
+    //    //根据角的定义，按照顺时针方向旋转而成的角叫做负角。逆时针旋转则是正角，没有旋转叫零角。
+    //    Vector3 rayPoint1 = new Vector3(
+    //        fireRange * Mathf.Cos(fieldOfAttackinRadians2),
+    //        0,
+    //        fireRange * Mathf.Sin(fieldOfAttackinRadians2));
+    //    rayPoint1 = transform.TransformPoint(rayPoint1);
+    //    Debug.DrawLine(transform.position, rayPoint1 + new Vector3(0, 1, 0), Color.blue);
+
+    //    //for (int i = 0; i < 11; i++)
+    //    //for (int i = 0; i<1; i++)
+    //    for (int i = 0; i < 10; i++)
+    //    {
+    //        RaycastHit hit;
+    //        //x = sin 如果从Y的正方向往下看所构成的2维平面(X,Z)，
+    //        //Z在垂直方向，X在水平方向
+    //        float angle = -fieldOfAttackinRadians + fieldOfAttackinRadians * 0.2f * (float)i;
+    //        Vector3 rayPoint = transform.TransformPoint(new Vector3(
+    //            fireRange * Mathf.Sin(angle),
+    //            0,
+    //            fireRange * Mathf.Cos(angle)));
+
+
+    //        if (i == 0)
+    //        {
+    //            Debug.DrawLine(transform.position + new Vector3(0, 1, 0), rayPoint + new Vector3(0, 1, 0), Color.green);
+    //        }
+    //        else
+    //        {
+    //            Debug.DrawLine(transform.position + new Vector3(0, 1, 0), rayPoint + new Vector3(0, 1, 0), Color.red);
+    //        }
+
+    //    }
+
+    //    Debug.DrawLine(transform.position, frontRayPoint + new Vector3(0, 1, 0), Color.black);
+    //}
+
+    public void AI_Arrive_TARGET_TRANGLE_Old(
+        GroupElement[] elments, E_FORMATION formation,
+        Vector3 startPoint, Vector3 endPoint, float speed,
+        MoveSteers.StopMoveCallback callback)
+    {
+        Vector3 dir = (endPoint - startPoint).normalized;
+        for (int i = 0; i < elments.Length; i++)
+        {
+            CharObj charObj = null;
+            charObj = BattleObjManager.Instance.BorrowCharObj(
+                elments[i].Type, elments[i].ServerEntityID, 0);
+            if (charObj == null)
+            {
+                LogMediator.LogError("角色对象charObj为空");
+            }
+            m_charObjs.Add(charObj);
+            //charObj.AI_Arrive(startPoint + dir * i * 15f, endPoint - dir * i * 15f, speed);
+        }
+        Vector3 offset = Vector3.zero;
+        //m_charObjs[0].AI_Arrive(startPoint, endPoint, speed);
+        //offset = dir;
+        //offset.x -= 15f;
+        //offset.z -= 15f;
+        //m_charObjs[1].AI_Arrive(startPoint, endPoint + offset, speed);
+        //offset = dir;
+        //offset.x += 15f;
+        //offset.z -= 15f;
+        //m_charObjs[2].AI_Arrive(startPoint, endPoint + offset, speed);
+        LogMediator.DrawLine(startPoint, endPoint, Color.red, (endPoint - startPoint).magnitude);
+        endPoint -= (dir * 15f);
+        LogMediator.DrawLine(startPoint, endPoint, Color.blue, (endPoint - startPoint).magnitude);
+
+
+        float deg = 45f;
+        float l = 15f;
+        float rad = Mathf.Deg2Rad * deg;
+        float r = Mathf.Cos(rad) * l;
+        float x = r * Mathf.Sin(rad);
+        float z = r * Mathf.Cos(rad);
+        endPoint = new Vector3(x, 0, z);
+        LogMediator.Log(endPoint);
+        LogMediator.DrawLine(startPoint, endPoint, Color.red, (endPoint - startPoint).magnitude);
+
+
+    }
+
     public void Release()
     {
         for(int i = 0; i < m_charObjs.Count; i++)
@@ -48,4 +168,7 @@ public class GroupController
         m_charObjs.Clear();
         m_charObjs = null;
     }
+
+
+
 }
--- War/Assets/Scripts/BattleLogic/Test/Test_AI_Group_1.cs
+++ War/Assets/Scripts/BattleLogic/Test/Test_AI_Group_1.cs
@@ -12,12 +12,11 @@ public class Test_AI_Group_1 : MonoBehaviour
 
     public void BtnClick()
     {
-        StartCoroutine("CreateCharObjs");
+        //CreateCharObjs_TARGET_VERTICAL_LINE();
     }
 
-    IEnumerator CreateCharObjs()
+    void CreateCharObjs_TARGET_VERTICAL_LINE()
     {
-        yield return null;
         GroupController groupController = new GroupController();
         GroupElement [] elements = new GroupElement[3];
         elements[0] = new GroupElement();
@@ -33,17 +32,88 @@ public class Test_AI_Group_1 : MonoBehaviour
         elements[2].Type = BattleObjManager.E_BATTLE_OBJECT_TYPE.M_ARM_TANK;
         Vector3 startPos = new Vector3(32f, 0f, 32f);
         //Vector3 endPos = new Vector3(Random.Range(10, 310), 0f, Random.Range(10, 310));
-        Vector3 endPos = new Vector3(160f, 0f, 160f);
+        Vector3 endPos = new Vector3(160f, 0f, 100f);
         //假定服务器规定这段路必须time秒走完
         float time = 15f;
         float speed = (endPos - startPos).magnitude / time;
-        groupController.AI_Arrive(elements, 
+
+        groupController.AI_Arrive_TARGET_VERTICAL_LINE(elements,
             GroupController.E_FORMATION.TARGET_VERTICAL_LINE,
             startPos, endPos, speed, null);
+
+        //groupController.AI_Arrive_TARGET_TRANGLE(elements,
+        //    GroupController.E_FORMATION.TARGET_TRANGLE,
+        //    startPos, endPos, speed, null);
+
+        
     }
 
-    void ArriveCallback()
+    void OnDrawGizmos()
     {
-        LogMediator.Log("ArriveCallback");
+        //Test_1();
+        Test_2();
+    }
+
+    void Test_1()
+    {
+        float fireRange = 32f;
+        Vector3 startPos = new Vector3(10, 0, 32);
+        Vector3 endPos = startPos + new Vector3(fireRange, 0f, 0f);
+        LogMediator.DrawLine(startPos, endPos, Color.red, (endPos - startPos).magnitude);
+        LogMediator.Log("dis 0 " + (endPos - startPos).magnitude);
+        float deg = 15f;
+        float radStart = Mathf.Deg2Rad * deg;
+        Vector3 rayPointStart = new Vector3(
+            fireRange * Mathf.Sin(radStart),
+            0,
+            fireRange * Mathf.Cos(radStart));
+        rayPointStart += (startPos - Vector3.zero);
+        LogMediator.DrawLine(startPos, rayPointStart, Color.blue, (rayPointStart - startPos).magnitude);
+        LogMediator.Log("dis 1 " + (rayPointStart - startPos).magnitude);
+
+        float radEnd = Mathf.Deg2Rad * (deg + 90f);
+        Vector3 rayPointEnd = new Vector3(
+            fireRange * Mathf.Sin(radEnd),
+            0,
+            fireRange * Mathf.Cos(radEnd));
+        rayPointEnd += (startPos - Vector3.zero);
+        LogMediator.DrawLine(startPos, rayPointEnd, Color.cyan, (rayPointEnd - startPos).magnitude);
+        LogMediator.Log("dis 2 " + (rayPointEnd - startPos).magnitude);
+    }
+
+    void Test_2()
+    {
+        //float fireRange = 32f;
+        Vector3 startPos = new Vector3(10, 0, 32);
+        Vector3 endPos = startPos + new Vector3(20, 0f, 40f);
+        float fireRange = (endPos - startPos).magnitude;
+        LogMediator.DrawLine(startPos, endPos, Color.red, (endPos - startPos).magnitude);
+        LogMediator.Log("dis 0 " + (endPos - startPos).magnitude);
+        float degStart = 60f;
+        float radStart = Mathf.Deg2Rad * degStart;
+        Vector3 rayPointStart = new Vector3(
+            fireRange * Mathf.Cos(radStart),
+            0,
+            fireRange * Mathf.Sin(radStart));
+        rayPointStart += (startPos - Vector3.zero);
+        LogMediator.DrawLine(startPos, rayPointStart, Color.blue, (rayPointStart - startPos).magnitude);
+        LogMediator.Log("dis 1 " + (rayPointStart - startPos).magnitude);
+
+        //float degEnd = Mathf.Acos((endPos - startPos).x / (endPos - startPos).magnitude);
+        //float radEnd = Mathf.Deg2Rad * (degStart + degStart);
+        float orginRad = Mathf.Acos((endPos - startPos).x / (endPos - startPos).magnitude);
+        float orginDeg = Mathf.Rad2Deg * orginRad;
+        LogMediator.Log("orginDeg " + orginDeg);
+        float degEnd = orginDeg + (orginDeg - degStart);
+        LogMediator.Log("degEnd " + degEnd);
+        float radEnd = Mathf.Deg2Rad * degEnd;
+
+        Vector3 rayPointEnd = new Vector3(
+            fireRange * Mathf.Cos(radEnd),
+            0,
+            fireRange * Mathf.Sin(radEnd));
+        rayPointEnd += (startPos - Vector3.zero);
+        LogMediator.DrawLine(startPos, rayPointEnd, Color.cyan, (rayPointEnd - startPos).magnitude);
+        LogMediator.Log("dis 2 " + (rayPointEnd - startPos).magnitude);
     }
 }
